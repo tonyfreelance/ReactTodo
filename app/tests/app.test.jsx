df3e7d70ea1c85f798502fbfe13d1ff3fe283c8f
Tony Nguyen <tony@tonyfreelance.com>
2016-05-22 15:21:41 +0000

    Fist commit

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,7 @@
+var expect = require("expect");
+
+describe('App', () => {
+    it('should prorperly run tests', () => {
+        expect(1).toBe(1);
+    });
+});
---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+var webpackConfig = require("./webpack.config.js");
+
+module.exports = function(config) {
+    config.set({
+        browsers: ['Chrome'],
+        singleRun: true,
+        frameworks: ['mocha'],
+        files: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/foundation-sites/dist/foundation.min.js',
+            'app/tests/**/*.test.jsx'
+        ],
+        preprocessors: {
+            'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+        },
+        reporters: ['mocha'],
+        client: {
+            mocha: {
+                timeout: '5000'
+            }
+        },
+        webpack: webpackConfig,
+        webpackServer: {
+            noInfo: true
+        },
+        hostname: process.env.IP,
+        port: process.env.PORT
+    });
+};
